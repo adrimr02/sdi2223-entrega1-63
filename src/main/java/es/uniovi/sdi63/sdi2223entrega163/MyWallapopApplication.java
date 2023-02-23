63b3277d25bf2eb6d34a689bde3f4bd0d68c3b5e
adrimr02 <adrianmartinezr2002@gmail.com>
2023-02-23 17:46:33 +0100

    Commit Inicial

---
+++ src/main/java/es/uniovi/sdi63/sdi2223entrega163/MyWallapopApplication.java
@@ -0,0 +1,13 @@
+package es.uniovi.sdi63.sdi2223entrega163;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWallapopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyWallapopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/es/uniovi/sdi63/sdi2223entrega163/Sdi2223Entrega163ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.uniovi.sdi63.sdi2223entrega163;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi2223Entrega163ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

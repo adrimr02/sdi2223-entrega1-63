326208c91545baf4f2c578ad8c5f46be7630b66d
Adrián Martínez <adrianmartinezr2002@gmail.com>
2023-03-14 12:07:26 +0100

    Corregido error al hacer login

--- src/main/java/es/uniovi/sdi63/sdi2223entrega163/controllers/UsersController.java
+++ src/main/java/es/uniovi/sdi63/sdi2223entrega163/controllers/UsersController.java
@@ -15,6 +15,10 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.security.Principal;
+import java.util.List;
 
 @Controller
 public class UsersController {
@@ -39,19 +43,20 @@ public class UsersController {
 
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String login() {
+    public String login(Model model) {
+        model.addAttribute("user", new User());
         return "login";
     }
 
     @RequestMapping(value = "/signup", method = RequestMethod.POST)
-    public String signup(@Validated User user, BindingResult result){
-        signUpFormValidator.validate(user,result);
-        if(result.hasErrors()) {
+    public String signup(@Validated User user, BindingResult result) {
+        signUpFormValidator.validate(user, result);
+        if (result.hasErrors()) {
             return "signup";
         }
         user.setRole(rolesService.getRoles()[1]);
         usersService.addUser(user);
-        //securityService.autoLogin(user.getEmail(),user.getPassword());
+        securityService.autoLogin(user.getEmail(),user.getPasswordConfirm());
         return "redirect:home";
     }
 
@@ -61,15 +66,16 @@ public class UsersController {
         return "signup";
     }
 
-/*
-    @RequestMapping(value = { "/home" }, method = RequestMethod.GET)
-    public String home(Model model) {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        String email = auth.getName();
-        User activeUser = usersService.getUserByEmail(email);
-        model.addAttribute("offerList", activeUser.getCreatedOffers());
-        model.addAttribute("usersList", usersService.getUsers());
-        return "home";
+    @RequestMapping("/login-success")
+    public String indexView(Principal principal) {
+        var user = usersService.getUserByEmail( principal.getName() );
+        if (user.getRole().equals( rolesService.getRoles()[0] )) {
+            return "redirect:/home";
+        } else {
+            return "redirect:/offer/my-offers";
+        }
     }
- */
+
 }
+
+

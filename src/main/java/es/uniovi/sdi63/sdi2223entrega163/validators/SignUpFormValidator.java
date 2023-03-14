bb3cc2832cf5bb841817201bb76be552419cc8ba
uo283794 <uo283794@uniovi.es>
2023-03-14 12:17:19 +0100

    Validacion Email remetido SignUp

--- src/main/java/es/uniovi/sdi63/sdi2223entrega163/validators/SignUpFormValidator.java
+++ src/main/java/es/uniovi/sdi63/sdi2223entrega163/validators/SignUpFormValidator.java
@@ -17,17 +17,9 @@ public class SignUpFormValidator implements Validator {
     public void validate(Object target, Errors errors) {
         User user = (User) target;
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "Error.empty");
-/*
-        if (user.getDni().length() < 5 || user.getDni().length() > 24) {
-            errors.rejectValue("dni", "Error.signup.dni.length");}
-*/
-
-       /* if (usersService.getUserByDni(user.getDni()) != null) {
-            errors.rejectValue("dni", "Error.signup.dni.duplicate");}
-        if (user.getName().length() < 5 || user.getName().length() > 24) {
-            errors.rejectValue("name", "Error.signup.name.length");}
-        if (user.getLastName().length() < 5 || user.getLastName().length() > 24) {
-            errors.rejectValue("lastName", "Error.signup.lastName.length");}*/
+        if(usersService.getUserByEmail(user.getEmail()) != null){
+            errors.rejectValue("email", "Error.signup.email.alradyExist");
+        }
         if (user.getPassword().length() < 5 || user.getPassword().length() > 24) {
             errors.rejectValue("password", "Error.signup.password.length");}
         if (!user.getPasswordConfirm().equals(user.getPassword())) {
